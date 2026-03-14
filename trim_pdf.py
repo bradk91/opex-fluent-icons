f33f05da308156ea7ce5573eea7ac161781bb7fe
Brad Klodowski <bradklodowski@gmail.com>
2026-03-14 12:57:10 -0400

    removed PDF folders

---
+++ trim_pdf.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+assets_dir = os.path.join(os.getcwd(), 'assets')
+
+for root, dirs, files in os.walk(assets_dir):
+    if 'PDF' in dirs:
+        pdf_path = os.path.join(root, 'PDF')
+        shutil.rmtree(pdf_path)
+        print(f"Removed: {pdf_path}")
